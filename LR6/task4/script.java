6931f7e208d60cbfc795e1e6e9e2fbd66cbf8799
prodbysendwxve@gmail.com <159626280+prodbysendwxve@users.noreply.github.com>
2024-04-19 13:59:15 +0300

    Add files via upload

---
+++ LR6/task4/script.java
@@ -0,0 +1,17 @@
+let resultField = document.getElementById('result');
+
+function appendValue(value) {
+    resultField.value += value;
+}
+
+function clearResult() {
+    resultField.value = '';
+}
+
+function calculate() {
+    try {
+        resultField.value = eval(resultField.value);
+    } catch (error) {
+        resultField.value = 'Error';
+    }
+}
